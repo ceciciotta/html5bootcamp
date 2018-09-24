18fbc3652e0c9975ecbd25604eb6c24e6fc0385d
ceciciotta <annciot23@gmail.com>
2018-09-24 19:31:16 -0300

    topic1arreglado

--- JAVASCRIPT/js/file2.js
+++ JAVASCRIPT/js/file2.js
@@ -61,30 +61,16 @@ function myRepofunction(params) {
         })
 }
 
-function search() {
-    let input = document.getElementById('text_search').value.toLowerCase(),
-        output = document.getElementById('id_texts').options;
-
-    for (let i = 0; i < output.length; i++) {
-        if (output[i].value.indexOf(input) == 0) {
-            output[i].selected = true;
-        }
-
-        if (document.forms[0].text_search.value == '') {
-            output[0].selected = true;
-        }
-    }
-}
-
 function genera_tabla() {
   var body = document.getElementsByTagName('body')[0];
   var tabla   = document.createElement('table');
   var tblBody = document.createElement('tbody');
-  for (var i = 0; i < 2; i++) {
+  for (var i = 0; i < tblBody.lenght ; i++) {
     var hilera = document.createElement('tr');
-    for (var j = 0; j < 2; j++) {
+    for (var j = 0; j < hilera.lenght ; j++) {
       var celda = document.createElement('td');
       var textoCelda = document.createTextNode('gel polish');
+      var textoCelda = document.createTextNode('regular polish');
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
     }
